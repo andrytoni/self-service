e153ff4ccc8c157ce2baab04b6755fdb44667d7d
Gustavo Leal <gustavopleal@outlook.com>
2022-08-15 22:20:03 -0300

    order schema updated

--- models/order.js
+++ models/order.js
@@ -1,10 +1,49 @@
-import mongoose, { isObjectIdOrHexString } from 'mongoose';
-import Product from '../../models/product.js';
+import mongoose from 'mongoose';
 
-const Orderschema = new mongoose.Schema({
-  _id: mongoose.Schema.Types.ObjectId,
-  product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product' },
-  quantity: { type: Number, default: 1 },
+const schema = new mongoose.Schema({
+  products: [
+    {
+      _id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Product',
+      },
+      name: { type: String },
+      price: { type: Number },
+      obs: { type: String, required: false },
+    },
+  ],
+  owner: {
+    _id: {
+      type: mongoose.Schema.Types.ObjectId,
+      unique: true,
+    },
+    table: {
+      type: Number,
+      required: false,
+    },
+  },
+  createdAt: {
+    type: Date,
+    default: new Date(),
+  },
+  status: {
+    type: Number,
+    //0 Pedido enviado
+    //1 Pedido aprovado
+    //2 Pedido em preparo
+    //3 Pedido pronto
+    //4 Pedido entregue
+    //5 Pedido finalizado
+    default: 0,
+  },
+  totalValue: {
+    type: Number,
+    required: true,
+  },
+  canceled: {
+    type: Boolean,
+    required: false,
+  },
 });
 
-export default mongoose.model('Order', Orderschema);
+export default mongoose.model('Order', schema);
