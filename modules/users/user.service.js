f8a0a6de32737c32a4ade0262459cab563a17577
Andre Brito <andre.brito.fernandes@gmail.com>
2022-09-20 19:57:14 -0300

    update validade password, find, uptadeUser, changePassword and  changeUserStatus

--- modules/users/user.service.js
+++ modules/users/user.service.js
@@ -1,58 +1,64 @@
 import mongoose from 'mongoose';
 
 const userService = (User) => {
-  const validatePassword = (password, email, name) => {
-    name = name.toUpperCase();
-    const partOfEmail = email.split('@')[0];
-    const partsOfName = name
-      .split(' ')
-      .map((nome) => password.toUpperCase().includes(nome));
-    let finalValidation = ['Password validations:'];
+  const getPasswordErrors = (password, email, name) => {
+    const uppercaseName = name.toUpperCase();
+    const uppercasePassword = password.toUpperCase();
+    const partOfEmail = email.split('@')[0].toUpperCase();
+    const partsOfName = uppercaseName.split(' ');
+    const passwordError = {
+      hasError: false,
+      errors: ['Password validations:'],
+    };
 
     if (password.length < 8) {
-      finalValidation.push('- At least 8 characteres');
+      passwordError.errors.push('- At least 8 characteres');
     }
     if (/[a-z]/.test(password) === false) {
-      finalValidation.push('- At least 1 lowercase character');
+      passwordError.errors.push('- At least 1 lowercase character');
     }
     if (/[A-Z]/.test(password) === false) {
-      finalValidation.push('- At least 1 uppercase character');
+      passwordError.errors.push('- At least 1 uppercase character');
     }
     if (/[0-9]/.test(password) === false) {
-      finalValidation.push('- At least 1 number');
+      passwordError.errors.push('- At least 1 number');
     }
     if (/[\u0020-\u002F\u003A-\u0040]/.test(password) === false) {
-      finalValidation.push('- At least 1 special character.');
+      passwordError.errors.push('- At least 1 special character.');
     }
-    if (password.includes(partOfEmail) === true) {
-      finalValidation.push("- Can't contain parts of email");
+    if (uppercasePassword.includes(partOfEmail) === true) {
+      passwordError.errors.push("- Can't contain parts of email");
     }
 
     for (let i = 0; i < partsOfName.length; i++) {
-      if (partsOfName[i] === true) {
-        finalValidation.push("- Can't contain parts of the name.");
-        break;
+      if (uppercasePassword.includes(partsOfName[i])) {
+        passwordError.errors.push("- Can't contain parts of the name.");
       }
     }
-    // finalValidation = finalValidation.join('\n');
 
-    return finalValidation;
+    if (passwordError.errors.length > 1) {
+      passwordError.hasError = true;
+      return passwordError;
+    } else {
+      return passwordError;
+    }
   };
 
   const createNewUser = async (user) => {
     if (!user) {
       throw new Error('Parameters are required');
     }
-    let passwordValidation = validatePassword(
+
+    let passwordErrors = getPasswordErrors(
       user.password,
       user.email,
       user.name
     );
 
-    if (passwordValidation.length > 1) {
-      passwordValidation = passwordValidation.join('\n');
-      throw new Error(passwordValidation);
+    if (passwordErrors.hasError === true) {
+      throw new Error(passwordErrors.errors.join('\n'));
     }
+
     return new User(user).save();
   };
 
@@ -60,6 +66,7 @@ const userService = (User) => {
     if (!email) {
       throw new Error('Email is required');
     }
+
     return User.findOne({ email: email });
   };
 
@@ -75,56 +82,74 @@ const userService = (User) => {
     const { name, role, isActive, date } = query;
     const finalQuery = {};
 
-    if (name) finalQuery.name = name;
-    if (role) finalQuery.role = role;
+    if (name) finalQuery.name = name.toUpperCase();
+    if (role) finalQuery.role = role.toUpperCase();
+    if (isActive) finalQuery.isActive = isActive === 'true';
     if (date) {
       const startDate = new Date(date);
       const finalDate = new Date(date);
       finalDate.setDate(startDate.getDate() + 1);
       finalQuery.createdAt = { $gte: startDate, $lt: finalDate };
     }
-    if (isActive) finalQuery.isActive = isActive;
 
     return User.find(finalQuery);
   };
 
   const updateUser = async (id, userUpdate) => {
     if (!id) {
-      throw new Error('ID is required');
+      throw new Error('ID is required.');
     }
     if (!userUpdate) {
-      throw new Error('Update parameter is required');
+      throw new Error('Update parameter is required.');
     }
-    if (userUpdate.password) return User.findByIdAndUpdate(id, userUpdate);
+    if (userUpdate.password) {
+      throw new Error('Password is not allowed to update.');
+    }
+    if (userUpdate.isActive) {
+      throw new Error('isActive status is not allowed to update.');
+    }
+
+    return User.findByIdAndUpdate(id, userUpdate);
   };
 
-  const changePassword = async (id, passwordUpdate) => {
+  const changePassword = async (id, passwordUpdate1, passwordUpdate2) => {
     if (!id) {
       throw new Error('ID is required');
     }
-    if (!passwordUpdate) {
-      throw new Error('New password is required');
+    if (!passwordUpdate1 || !passwordUpdate2) {
+      throw new Error('New password is required.');
     }
-    let user = await findById(id);
+    if (passwordUpdate1 !== passwordUpdate2) {
+      throw new Error('Passwords should match.');
+    }
 
-    let passwordValidation = validatePassword(
-      passwordUpdate,
+    let user = await findById(id);
+    if (user == null) {
+      throw new Error('User not found');
+    }
+
+    let passwordErrors = getPasswordErrors(
+      passwordUpdate1,
       user.email,
       user.name
     );
 
-    if (passwordValidation.length > 1) {
-      passwordValidation = passwordValidation.join('\n');
-      throw new Error(passwordValidation);
+    if (passwordErrors.hasError === true) {
+      throw new Error(passwordErrors.errors.join('\n'));
     }
-    return User.findByIdAndUpdate(id, { password: passwordUpdate });
+
+    return User.findByIdAndUpdate(id, { password: passwordUpdate1 });
   };
 
   const changeUserStatus = async (id) => {
     if (!id) {
       throw new Error('ID is required');
     }
+
     let user = await findById(id);
+    if (user == null) {
+      throw new Error('User not found');
+    }
 
     if (user.isActive === true) {
       return User.findByIdAndUpdate(id, { isActive: false });
